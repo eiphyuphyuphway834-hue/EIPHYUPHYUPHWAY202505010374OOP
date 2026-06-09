3b0d0303ceb7ff53c4d8553a5e6492c5c62162f2
eiphyuphyuphway834-hue <eiphyuphyuphway834@gmail.com>
2026-06-09 07:47:14 +0000

    Student Class

---
+++ Tutorial_2/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("mary", 23, 3.33);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
